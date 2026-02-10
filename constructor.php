1f1d02f9a9ca0381c20e540f8deef219c2e51656
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2026-02-10 08:50:37 +0700

    constructor

---
+++ constructor.php
@@ -0,0 +1,39 @@
+<?php 
+
+// jualan produk
+// komik
+// game
+
+
+class Produk {
+    public $judul, 
+           $penulis,
+           $penerbit,
+           $harga;
+
+    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+}
+
+$produk1 = new Produk("Naruto", "Masashi Kisomoto", "Shonen Jump", 250000);
+$produk2 = new Produk("Uncharted", "Neil Druckman", "Sony Computer", 500000);
+$produk3 = new Produk("DragonBall");
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
+echo "<br>";
+var_dump($produk3);
+
+
+
+?>
